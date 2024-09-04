b3d60c6dfe75f9c1c869011d833ec56aa7a5f763
Alcibiades Athens <alcibiades.eth@protonmail.com>
2024-09-04 19:22:26 -0400

    feat: basic toy http1 server

--- src/lib.rs
+++ src/lib.rs
@@ -1,130 +1,46 @@
-//! hyper-server is a [hyper] server implementation designed to be used with [axum] framework.
-//!
-//! # Features
-//!
-//! - HTTP/1 and HTTP/2
-//! - HTTPS through [rustls] or [openssl].
-//! - High performance through [hyper].
-//! - Using [tower] make service API.
-//! - Very good [axum] compatibility. Likely to work with future [axum] releases.
-//!
-//! # Guide
-//!
-//! hyper-server can [`serve`] items that implement [`MakeService`] with some additional [trait
-//! bounds](crate::service::MakeServiceRef). Make services that are [created] using [`axum`]
-//! complies with those trait bounds out of the box. Therefore it is more convenient to use this
-//! crate with [`axum`].
-//!
-//! All examples in this crate uses [`axum`]. If you want to use this crate without [`axum`] it is
-//! highly recommended to learn how [tower] works.
-//!
-//! [`Server::bind`] or [`bind`] function can be called to create a server that will bind to
-//! provided [`SocketAddr`] when [`serve`] is called.
-//!
-//! A [`Handle`] can be passed to [`Server`](Server::handle) for additional utilities like shutdown
-//! and graceful shutdown.
-//!
-//! [`bind_rustls`] can be called by providing [`RustlsConfig`] to create a HTTPS [`Server`] that
-//! will bind on provided [`SocketAddr`]. [`RustlsConfig`] can be cloned, reload methods can be
-//! used on clone to reload tls configuration.
-//!
-//! # Features
-//!
-//! * `tls-rustls` - activate [rustls] support.
-//! * `tls-openssl` - activate [openssl] support.
-//!
-//! # Example
-//!
-//! A simple hello world application can be served like:
-//!
-//! ```rust,no_run
-//! use axum::{routing::get, Router};
-//! use std::net::SocketAddr;
-//!
-//! #[tokio::main]
-//! async fn main() {
-//!     let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-//!
-//!     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-//!     println!("listening on {}", addr);
-//!     hyper_server::bind(addr)
-//!         .serve(app.into_make_service())
-//!         .await
-//!         .unwrap();
-//! }
-//! ```
-//!
-//! You can find more examples in [repository].
-//!
-//! [axum]: https://crates.io/crates/axum
-//! [bind]: crate::bind
-//! [bind_rustls]: crate::bind_rustls
-//! [created]: https://docs.rs/axum/0.3/axum/struct.Router.html#method.into_make_service
-//! [hyper]: https://crates.io/crates/hyper
-//! [openssl]: https://crates.io/crates/openssl
-//! [repository]: https://github.com/valorem-labs-inc/hyper-server/examples
-//! [rustls]: https://crates.io/crates/rustls
-//! [tower]: https://crates.io/crates/tower
-//! [`axum`]: https://docs.rs/axum/0.3
-//! [`serve`]: crate::server::Server::serve
-//! [`MakeService`]: https://docs.rs/tower/0.4/tower/make/trait.MakeService.html
-//! [`RustlsConfig`]: crate::tls_rustls::RustlsConfig
-//! [`SocketAddr`]: std::net::SocketAddr
+use std::{convert::Infallible, net::SocketAddr};
 
-#![forbid(unsafe_code)]
-#![warn(
-    clippy::await_holding_lock,
-    clippy::cargo_common_metadata,
-    clippy::dbg_macro,
-    clippy::doc_markdown,
-    clippy::empty_enum,
-    clippy::enum_glob_use,
-    clippy::inefficient_to_string,
-    clippy::mem_forget,
-    clippy::mutex_integer,
-    clippy::needless_continue,
-    clippy::todo,
-    clippy::unimplemented,
-    clippy::wildcard_imports,
-    future_incompatible,
-    missing_docs,
-    missing_debug_implementations,
-    unreachable_pub
-)]
-#![cfg_attr(docsrs, feature(doc_cfg))]
-
-mod addr_incoming_config;
-mod handle;
-mod http_config;
-mod notify_once;
-mod server;
-
-pub mod accept;
-pub mod service;
-
-pub use self::{
-    addr_incoming_config::AddrIncomingConfig,
-    handle::Handle,
-    http_config::HttpConfig,
-    server::{bind, from_tcp, Server},
+use http_body_util::Full;
+use hyper::{
+    body::{Bytes, Incoming},
+    Request,
+    Response, server::conn::http1,
 };
+use hyper_util::{rt::TokioIo, service::TowerToHyperService};
+use tokio::net::TcpListener;
+use tower::{Service, ServiceBuilder};
 
-#[cfg(feature = "tls-rustls")]
-#[cfg_attr(docsrs, doc(cfg(feature = "tls-rustls")))]
-pub mod tls_rustls;
+// Define just a simple hello world endpoint that always responds with "Hello, World!"
+// and is some sort of really stripped down request/response handler.
+async fn hello(_: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
 
-#[doc(inline)]
-#[cfg(feature = "tls-rustls")]
-pub use self::tls_rustls::export::{bind_rustls, from_tcp_rustls};
+async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // Bind 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::spawn(async move {
+            // This is an implementation of a tower service
+            // Here you might use a tower "service of services" to route requests
+            let svc = tower::service_fn(hello);
+            // Wrap it in a service builder
+            let svc = ServiceBuilder::new().service(svc);
+            // Convert it to hyper service
+            let svc = TowerToHyperService::new(svc);
+            // Serve http1 connections
+            if let Err(err) = http1::Builder::new().serve_connection(io, svc).await {
+                eprintln!("server error: {}", err);
+            }
+        });
+    }
+}
 
-#[cfg(feature = "tls-openssl")]
-#[cfg_attr(docsrs, doc(cfg(feature = "tls-openssl")))]
-pub mod tls_openssl;
-
-#[doc(inline)]
-#[cfg(feature = "tls-openssl")]
-pub use self::tls_openssl::bind_openssl;
-
-#[cfg(feature = "proxy-protocol")]
-#[cfg_attr(docsrs, doc(cfg(feature = "proxy_protocol")))]
-pub mod proxy_protocol;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    run().await
+}
